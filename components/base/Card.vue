f27dc4cdad590deedc451cd9414d8918febe099b
Ismael Costa <ismaelac96@icloud.com>
2023-12-13 21:33:17 -0300

    feat: create card component

---
+++ components/base/Card.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+interface Props {
+  size?: 'small' | 'medium' | 'large';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 'medium',
+})
+
+</script>
+
+<template>
+  <div class="card" :class="size">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: var(--spacing-small) var(--color-border) solid;
+  border-radius: var(--spacing-medium);
+  padding: var(--spacing-large);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--spacing-xxl);
+  background-color: var(--color-white)
+}
+
+.small {
+  width: 15rem;
+}
+
+.medium {
+  width: 20rem;
+}
+
+.large {
+  width: 25rem;
+}
+</style>
