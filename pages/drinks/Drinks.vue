d6bea8fb42deaf3a2516faa27e099ae49bb99f03
Ismael Costa <ismaelac96@icloud.com>
2023-12-14 19:45:44 -0300

    feat: add table to drinks age

--- pages/drinks/Drinks.vue
+++ pages/drinks/Drinks.vue
@@ -3,11 +3,41 @@ import { useLayoutsDefaultStore } from '@/store/layouts/default.store'
 
 const store = useLayoutsDefaultStore()
 
-store.title = 'Categorias'
+const route = useRoute()
+
+store.title = 'Bebidas / ' + route.params.drink
+
+const headers = [
+  'Bebida',
+  'Descrição',
+]
+
+const data = [
+  [
+    {
+      content: 'Destilados',
+      link: '/',
+    },
+    {
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend at mi eu placerat. In ac velit vitae augue rutrum porta. Phasellus porta velit orci, iaculis ullamcorper nisl porta in.',
+    }
+  ],
+  [
+    {
+      content: 'Vinhos',
+      link: '/',
+    },
+    {
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend at mi eu placerat. In ac velit vitae augue rutrum porta. Phasellus porta velit orci, iaculis ullamcorper nisl porta in.',
+    }
+  ],
+]
 </script>
 
 <template>
-  <NuxtLayout>Categories</NuxtLayout>
+  <NuxtLayout>
+    <BaseTable :headers="headers" :data="data"/>
+  </NuxtLayout>
 </template>
 
 <style>
