5234f82600ad12b5edec151411cbd7dff495963a
Ismael Costa <ismaelac96@icloud.com>
2023-12-14 23:50:01 -0300

    feat: improve style of login page

--- pages/login/Login.vue
+++ pages/login/Login.vue
@@ -1,17 +1,24 @@
+<script setup lang="ts">
+const email = ref('');
+const password = ref('');
+
+const isDisabled = computed(() => !email.value || !password.value)
+</script>
+
 <template>
   <NuxtLayout name="empty">
     <div class="base-card__container">
-      <BaseCard size="large">
+      <BaseCard size="medium" class="base-card">
         <template #header>logotipo</template>
         <template #default>
           <BaseForm>
-            <BaseInputText label="E-mail:" />
-            <BaseInputText label="Senha:" />
+            <BaseInputText label="E-mail:" v-model="email"/>
+            <BaseInputText label="Senha:" v-model="password" />
           </BaseForm>
         </template>
         <template #footer>
           <div class="buttons__container">
-            <BaseButton>Entrar</BaseButton>
+            <BaseButton :disabled="isDisabled">Entrar</BaseButton>
             <BaseButton type="link" size="small">Cadastre-se</BaseButton>
           </div>
         </template>
@@ -30,13 +37,18 @@
   background-color: var(--color-primary);
 }
 
-.base-card__container>* {
+.base-card__container > * {
   margin: 0 auto;
+}
+
+.base-card {
+  padding: var(--spacing-xl)
 }
 
 .buttons__container {
   display: flex;
   flex-direction: column;
   gap: var(--spacing-md);
+  align-items: center;
 }
 </style>
