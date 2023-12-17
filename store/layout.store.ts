4fadc17d3cac19e0e3383fa9a04ad2ff8f9ebb62
Ismael Costa <ismaelac96@icloud.com>
2023-12-16 23:12:02 -0300

    style: change store names

--- store/layout.store.ts
+++ store/layout.store.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia"
 
-export const useLayoutsDefaultStore = defineStore('counter', () => {
+export const useLayoutStore = defineStore('counter', () => {
   const title = ref('')
   const search = ref('')
   const backLink = ref('')
