e2019708a4d094cb056a7c17d7541a8dbdbb84cc
Ismael Costa <ismaelac96@icloud.com>
2023-12-16 23:11:43 -0300

    feat: creation of types

---
+++ components/base/table/base-table.types.ts
@@ -0,0 +1,6 @@
+export interface TableData {
+  content: string;
+  link?: string;
+}
+
+export type TableRow = [TableData, TableData]
---
+++ types/drink.types.ts
@@ -0,0 +1,8 @@
+export interface Drink {
+  id: string;
+  name: string;
+  description: string;
+  categoryId: string;
+  created_at: string;
+  updated_at: string;
+}
